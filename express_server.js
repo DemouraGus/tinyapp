9b549e8a082d3746827aaef95da47a8907bb7eab
DemouraGus <demoura.gus@gmail.com>
2024-11-25 08:47:27 -0800

    urls.json response added

--- express_server.js
+++ express_server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-const urlDatavase = {
+const urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
 
 app.get('/', (request, response) => {
   response.send('Hello!');
+});
+
+app.get('urls.json', (request, response) => {
+  response.json(urlDatabase);
 });
 
 app.listen(port, () => {
